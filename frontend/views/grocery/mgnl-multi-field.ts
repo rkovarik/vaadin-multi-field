0b0ffe5a7aaf753f5a33dfb0180e968ada7b0cab
rkovarik <roman.kovarik@magnolia-cms.com>
2022-09-08 12:51:28 +0200

    SQ

--- frontend/views/grocery/mgnl-multi-field.ts
+++ frontend/views/grocery/mgnl-multi-field.ts
@@ -4,8 +4,6 @@ import '@vaadin/custom-field';
 import '@vaadin/text-field'
 import '@vaadin/vertical-layout'
 import {TextField} from "@vaadin/text-field";
-import {VerticalLayout} from "@vaadin/vertical-layout";
-import {Button} from "@vaadin/button";
 
 @customElement('mgnl-multi-field')
 export class GroceryView extends LitElement {
@@ -19,31 +17,17 @@ export class GroceryView extends LitElement {
                         <vaadin-icon icon="vaadin:phone"></vaadin-icon>
                     </vaadin-button>
                 </vaadin-horizontal-layout>
-                <vaadin-button @click="${this.getOnclick}">+</vaadin-button>
+                <vaadin-button id="add-field-button" @click="${this.getOnclick}">+</vaadin-button>
             </vaadin-vertical-layout>`;
     }
 
-    render2() {
-        let layout = new VerticalLayout();
-        layout.append(this.getElementsByTagName("slot")[0])
-        this.assignedSlot
-        // let nodes = html`<slot></slot>`;
-        // layout.append(this.assignedSlot)
-        // this.slot
-        let button = new Button();
-        button.innerText = "+";
-        // button.onclick = this.getOnclick(button);
-        layout.appendChild(button);
-        return layout;
-    }
-
     private getOnclick() {
-        let newField = new TextField();
-        // this.getElementsByClassName("slot").item(0)
-        console.warn(newField)
+        let newField = this.firstElementChild?.parentElement;
+        let addButton = this.shadowRoot?.getElementById("add-field-button"); //.getElementsByTagName("vaadin-horizontal-layout");
+        console.warn(addButton)
         // this.shadowRoot?.querySelector("#slot")?.childNodes.item(0);
         if (newField) {
-            this.appendChild(newField);
+            addButton?.before(newField);
             // newField.focus()
         }
     }
